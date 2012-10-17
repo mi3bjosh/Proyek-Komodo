1dc1d2bab2584b6605e516d0615a51380c12bda8
taxcool <mmtaxcool9@gmail.com>
2012-10-18 01:50:23 +0700

    Miftah -> ganti password db
    password db diganti default agar bisa diakses semua pekerja

--- system/application/config/database.php
+++ system/application/config/database.php
@@ -39,7 +39,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "choirul7";
+$db['default']['password'] = "";
 $db['default']['database'] = "elearning-komodo";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
