76f2d20ed70bf8ddfc1b4d8b14545dc7bc452ff0
nabilla <nabilla.nabb@rocketmail.com>
2012-10-30 09:17:32 +0700

    tambah function hapus, getId by Billa
    tambah function hapus, getId by NaBilla

--- system/application/models/shoutmodel.php
+++ system/application/models/shoutmodel.php
@@ -14,15 +14,6 @@ return $data;
 }
 }
 
-function getcom(){
-$comments = $this->db->get('comments');
-if($comments->num_rows() > 0){foreach ($comments->result() as $shout){
-$data[] = $comments;
-}
-return $data;
-}
-}
-
 
 function newshout(){
 $name=$_POST["name"];
@@ -34,6 +25,31 @@ $ins_shout = array(
 'email'=>$email
 );
 $this->db->insert('shouts',$ins_shout);
+}
+
+function edit($id){
+  $shout = $this->input->post('shout');
+	$email = $this->input->post('email');
+	$data = array(
+			'shout' => $shout,
+			'email' => $email
+			);
+	$this->db->where('id',$id);
+	$query = $this->db->update('shouts',$data);
+	return $query;
+}
+
+function hapus($id)
+{	
+	$this->db->where('id',$id);
+	$query = $this->db->delete('shouts');
+	return $query;
+}
+
+function GetId($id){
+$this->db->where('id',$id);
+$shouts = $this->db->get('shouts');
+return $shouts;
 }
 }
 ?>
